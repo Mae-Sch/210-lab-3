bfa78483d91ee4f9422c731e5a528ab62be9ef8a
Mae-Sch <maeschw@proton.me>
2026-09-01 15:52:08 -0700

    set up restaurant struct

--- restaurantStructPractice.cpp
+++ restaurantStructPractice.cpp
@@ -1,2 +1,18 @@
+#include <iostream>
 
+using namespace std;
+
+struct restaurant {
+    string name;
+    string address;
+    string cuisine;
+    bool isChain;
+    float rating; //rating out of 5
+
+};
+
+int main () {
+
+    return 0;
+}
 
